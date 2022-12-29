15d0e24c719e4eb1dbd07afa7ccdb31cece5d46f
sanches311 <itchipset@gmail.com>
2022-12-29 21:52:18 +0300

    fix: layot mainpage cartpage 404page

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,10 @@
 import '../style/app.scss';
 import { initRoute } from './components/router';
 import Listen from './components/listen';
+import logoImg from '../assets/img/logo.png';
 
 initRoute();
 Listen.listenselectBook();
+
+const logo = document.querySelector('.logo-img') as HTMLImageElement;
+logo!.src = logoImg;
