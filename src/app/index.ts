a20a5d1077044cdfa99749e374f80c94c6e1ba10
sanches311 <itchipset@gmail.com>
2022-12-29 22:07:34 +0300

    feat: add function loadLogo

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,8 @@
 import '../style/app.scss';
 import { initRoute } from './components/router';
 import Listen from './components/listen';
-import logoImg from '../assets/img/logo.png';
+import { loadLogo } from './components/utils';
 
 initRoute();
 Listen.listenselectBook();
-
-const logo = document.querySelector('.logo-img') as HTMLImageElement;
-logo!.src = logoImg;
+loadLogo();
