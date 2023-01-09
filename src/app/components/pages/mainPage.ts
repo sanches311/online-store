73b866047678439a5210cb1a7d8ab1c07e0bf5cf
sanches311 <itchipset@gmail.com>
2023-01-09 22:17:13 +0300

    feat: final

--- src/app/components/pages/mainPage.ts
+++ src/app/components/pages/mainPage.ts
@@ -195,165 +195,165 @@ export default class MainPage {
     }
 
     getMainPageHtml() {
-        return `<main class="main-page">
-        <div class="container">
-          <section class="all-filters">
-            <div class="all-filters__tasks-button">
-              <button class="reset">Reset filters</button>
-              <button class="copy">Copy link</button>
-            </div>
-            <div class="all-filters__category">
-              <h3 class="category__title">Разделы</h3>
-              <ul class="category">
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Книги на белорусском языке</p>
-                  <div id='filter-bel-count'class="filter-matching">${this.getBooksCount().belorussian.countAll}/ ${
-            this.getBooksCount().belorussian.countCurr
-        }</div>
-                </li>
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Иностранная литература</p>
-                  <div id='filter-for-count' class="filter-matching">${this.getBooksCount().foreign.countAll}/ ${
-            this.getBooksCount().foreign.countCurr
-        } </div>
-                </li>
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Компьютерная литература</p>
-                  <div id='filter-comp-count' class="filter-matching">${this.getBooksCount().computer.countAll}/ ${
-            this.getBooksCount().computer.countCurr
-        }</div>
-                </li>
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Научная литература</p>
-                  <div id='filter-sci-count' class="filter-matching">${this.getBooksCount().scientific.countAll}/ ${
-            this.getBooksCount().scientific.countCurr
-        }</div>
-                </li>
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Художественная литература</p>
-                  <div id='filter-art-count' class="filter-matching">${this.getBooksCount().artistic.countAll}/ ${
-            this.getBooksCount().artistic.countCurr
-        }</div>
-                </li>
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Бизнес литература</p>
-                  <div id='filter-bus-count' class="filter-matching">${this.getBooksCount().business.countAll}/ ${
+      return `<main class="main-page">
+      <div class="container">
+        <section class="all-filters">
+          <div class="all-filters__tasks-button">
+            <button class="reset">Reset filters</button>
+            <button class="copy">Copy link</button>
+          </div>
+          <div class="all-filters__category">
+            <h3 class="category__title">Разделы</h3>
+            <ul class="category">
+              <li class="categoty__item">
+                <input name='belorussian' class='category-book' type="checkbox">
+                <p>Книги на белорусском языке</p>
+                <div id='filter-bel-count'class="filter-matching">${this.getBooksCount().belorussian.countAll}/ ${
+          this.getBooksCount().belorussian.countCurr
+      }</div>
+              </li>
+              <li class="categoty__item">
+                <input name='foreign' class='category-book' type="checkbox">
+                <p>Иностранная литература</p>
+                <div id='filter-for-count' class="filter-matching">${this.getBooksCount().foreign.countAll}/ ${
+          this.getBooksCount().foreign.countCurr
+      } </div>
+              </li>
+              <li class="categoty__item">
+                <input name='computer' class='category-book' type="checkbox">
+                <p>Компьютерная литература</p>
+                <div id='filter-comp-count' class="filter-matching">${this.getBooksCount().computer.countAll}/ ${
+          this.getBooksCount().computer.countCurr
+      }</div>
+              </li>
+              <li class="categoty__item">
+                <input name='scientific' class='category-book' type="checkbox">
+                <p>Научная литература</p>
+                <div id='filter-sci-count' class="filter-matching">${this.getBooksCount().scientific.countAll}/ ${
+          this.getBooksCount().scientific.countCurr
+      }</div>
+              </li>
+              <li class="categoty__item">
+                <input name='artistic' class='category-book' type="checkbox">
+                <p>Художественная литература</p>
+                <div id='filter-art-count' class="filter-matching">${this.getBooksCount().artistic.countAll}/ ${
+          this.getBooksCount().artistic.countCurr
+      }</div>
+              </li>
+              <li class="categoty__item">
+                <input name='business' class='category-book' type="checkbox">
+                <p>Бизнес литература</p>
+                <div id='filter-bus-count' class="filter-matching">${this.getBooksCount().business.countAll}/ ${
             this.getBooksCount().business.countCurr
         }</div>
-                </li>
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Детская литература</p>
-                  <div id='filter-chil-count' class="filter-matching">${this.getBooksCount().children.countAll}/ ${
+              </li>
+              <li class="categoty__item">
+                <input name='children' class='category-book' type="checkbox">
+                <p>Детская литература</p>
+                <div id='filter-chil-count' class="filter-matching">${this.getBooksCount().children.countAll}/ ${
             this.getBooksCount().children.countCurr
         }</div>                
-              </ul>
-            </div>
-            <div class="all-filters__category">
-              <h3 class="category__title">Наличие</h3>
-              <ul class="category">
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>На складе</p>
-                  <div id='filter-stok-count' class="filter-matching">${this.getBooksCount().stok.countAll}/ ${
+            </ul>
+          </div>
+          <div class="all-filters__category">
+            <h3 class="category__title">Наличие</h3>
+            <ul class="category">
+              <li class="categoty__item">
+                <input  name='stok' class='quantity-book' type="checkbox">
+                <p>На складе</p>
+                <div id='filter-stok-count' class="filter-matching">${this.getBooksCount().stok.countAll}/ ${
             this.getBooksCount().stok.countCurr
         }</div>
-                </li>
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Под заказ</p>
-                  <div id='filter-stokOrd-count' class="filter-matching">${this.getBooksCount().stok.countOrderAll}/ ${
+              </li>
+              <li class="categoty__item">
+                <input name='order' class='quantity-book' type="checkbox">
+                <p>Под заказ</p>
+                <div id='filter-stokOrd-count' class="filter-matching">${this.getBooksCount().stok.countOrderAll}/ ${
             this.getBooksCount().stok.countOrderCurr
         }</div>
-                </li>
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Нет в наличии</p>
-                  <div id='filter-stokNoOrd-count' class="filter-matching">${this.getBooksCount().stok.countNoAll}/ ${
+              </li>
+              <li class="categoty__item">
+                <input name='stokNo' class='quantity-book' type="checkbox">
+                <p>Нет в наличии</p>
+                <div id='filter-stokNoOrd-count' class="filter-matching">${this.getBooksCount().stok.countNoAll}/ ${
             this.getBooksCount().stok.countNoCurr
         }</div>
-                </li>
-              </ul>
-            </div>
-            <div class="all-filters__category">
-              <h3 class="category__title">Обложка</h3>
-              <ul class="category">
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Мягкая обложка</p>
-                  <div id='filter-soft-count' class="filter-matching">${this.getBooksCount().type.countSoftAll}/ ${
+              </li>
+            </ul>
+          </div>
+          <div class="all-filters__category">
+            <h3 class="category__title">Обложка</h3>
+            <ul class="category">
+              <li class="categoty__item">
+                <input name='soft' class='type-book' type="checkbox">
+                <p>Мягкая обложка</p>
+                <div id='filter-soft-count' class="filter-matching">${this.getBooksCount().type.countSoftAll}/ ${
             this.getBooksCount().type.countSoftCurr
         }</div>
-                </li>
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Твёрдый переплёт</p>
-                  <div id='filter-hard-count' class="filter-matching">${this.getBooksCount().type.countHardAll}/ ${
+              </li>
+              <li class="categoty__item">
+                <input name='hard' class='type-book' type="checkbox">
+                <p>Твёрдый переплёт</p>
+                <div id='filter-hard-count' class="filter-matching">${this.getBooksCount().type.countHardAll}/ ${
             this.getBooksCount().type.countHardCurr
         }</div>
-                </li>
-                <li class="categoty__item">
-                  <input type="checkbox">
-                  <p>Интегральный переплёт</p>
-                  <div id='filter-int-count' class="filter-matching">${this.getBooksCount().type.countIntAll}/ ${
+              </li>
+              <li class="categoty__item">
+                <input name='integral' class='type-book' type="checkbox">
+                <p>Интегральный переплёт</p>
+                <div id='filter-int-count' class="filter-matching">${this.getBooksCount().type.countIntAll}/ ${
             this.getBooksCount().type.countIntCurr
         }</div>
-                </li>
-              </ul>
+              </li>
+            </ul>
+          </div>
+          <div class="all-filters__category">
+            <h3 class="category__title">Год издания</h3>
+            <div class="min-max">
+              <span class="min">2018</span>
+              <p>-</p>
+              <span class="max">2022</span>
             </div>
-            <div class="all-filters__category">
-              <h3 class="category__title">Год издания</h3>
-              <div class="min-max">
-                <span class="min">2018</span>
-                <p>-</p>
-                <span class="max">2022</span>
+            <div class="slider">
+              <input type="range" min="0" max="5" step="1" value="0">
+            </div>
+          </div>
+          <div class="all-filters__category">
+            <h3 class="category__title">Цена</h3>
+            <div class="min-max">
+              <span class="min">0</span>
+              <p>-</p>
+              <span class="max">1000</span>
+            </div>
+            <div class="slider">
+              <input type="range" min="0" max="1000" step="10" value="0">
+            </div>
+          </div>
+        </section>
+        <section class="all-products">
+          <div class="all-products__header">
+            <div class="select">
+              ${this.getSelectOption()}
+            </div>
+            <h3>Found: <span class="found">${store.books.length}</span></h3>
+            <div class="search">
+              <input id='live-search' type="text">
+            </div>
+            <div class="display">
+              <div class="col-2">
+                <span class="material-symbols-outlined">drag_indicator</span>
               </div>
-              <div class="slider">
-                <input type="range" min="0" max="5" step="1" value="0">
+              <div class="col-3">
+                <span class="material-symbols-outlined">apps</span>
               </div>
             </div>
-            <div class="all-filters__category">
-              <h3 class="category__title">Цена</h3>
-              <div class="min-max">
-                <span class="min">0</span>
-                <p>-</p>
-                <span class="max">1000</span>
-              </div>
-              <div class="slider">
-                <input type="range" min="0" max="1000" step="10" value="0">
-              </div>
-            </div>
-          </section>
-          <section class="all-products">
-            <div class="all-products__header">
-              <div class="select">
-                ${this.getSelectOption()}
-              </div>
-              <h3>Found: <span class="found">${store.books.length}</span></h3>
-              <div class="search">
-                <input id='live-search' type="text">
-              </div>
-              <div class="display">
-                <div class="col-2">
-                  <span class="material-symbols-outlined">drag_indicator</span>
-                </div>
-                <div class="col-3">
-                  <span class="material-symbols-outlined">apps</span>
-                </div>
-              </div>
-            </div>
-            <div class="all-products__container">     
-            ${this.getBooksHtml()}
-              </div> 
-            </div>
-          </section>
-        </div>
-      </main>`;
+          </div>
+          <div class="all-products__container">     
+          ${this.getBooksHtml()}
+            </div> 
+          </div>
+        </section>
+      </div>
+    </main>`;
     }
 }
